55e21b0a0a2c42a9942a9dd4c3376d664158d283
Dhaval <dhavallakhaniofficial@gmail.com>
2025-11-16 19:31:46 +0530

    Initial Commit for ProjectU11

---
+++ src/u03/U3_8_LaunchingBrowser.java
@@ -0,0 +1,20 @@
+package u03;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+
+public class U3_8_LaunchingBrowser {
+
+	public static void main(String[] args) {
+				
+		WebDriver driver0 = new ChromeDriver();
+//		ChromeDriver driver1 = new ChromeDriver();
+//		FirefoxDriver driver2 = new FirefoxDriver();
+		
+		driver0.get("https://www.codenbox.com");
+		
+		
+		
+	}
+	
+}
